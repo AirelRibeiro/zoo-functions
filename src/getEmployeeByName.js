c765f0c4576739a494d83ee5bb499b4945abb676
Airel <mv.ribeirosilva@gmail.com>
2022-03-16 16:18:33 -0300

    Requisito 3, feito (suspeitosamente simples) 🐆

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
+console.log(employees);
+
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (!employeeName) {
+    return {};
+  }
+  return employees.find((emp) => emp.firstName === employeeName || emp.lastName === employeeName);
 }
 
 module.exports = getEmployeeByName;
