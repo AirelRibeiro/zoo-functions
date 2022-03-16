bd84aa4e854fb0d26c57f0e70f18118d7f73c908
Airel <mv.ribeirosilva@gmail.com>
2022-03-16 15:44:25 -0300

    Requisito 1, onde tive que aprender a ser gente

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+const { species } = data;
+
+function getSpeciesByIds(...identidades) {
+  const ids = identidades;
+  const result = [];
+  ids.forEach((id) => {
+    result.push(species.find((animal) => animal.id === id));
+  });
+  return result;
 }
+
+// console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce', '533bebf3-6bbe-41d8-9cdf-46f7d13b62ae'));
 
 module.exports = getSpeciesByIds;
