22155a6fafbc2b988c782093af45c9a2af2520e5
Airel <mv.ribeirosilva@gmail.com>
2022-03-16 19:44:41 -0300

    Requisito 5, finalmente🦛🦥

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,25 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    const objeto = {};
+    species.forEach((spc) => {
+      objeto[spc.name] = spc.residents.length;
+    });
+    return objeto;
+  }
+  const anm = animal.specie;
+  const anms = species.find((spc) => spc.name === anm);
+  if (animal.sex) {
+    const anmSex = animal.sex;
+    const anmSexs = anms.residents.filter((res) => res.sex === anmSex);
+    return anmSexs.length;
+  }
+  return anms.residents.length;
 }
+
+console.log(countAnimals());
 
 module.exports = countAnimals;
