18c43261d9be579ca44a6833802e24fb230af5c2
Airel <mv.ribeirosilva@gmail.com>
2022-03-16 18:11:53 -0300

    Requisito 4 🦌🦏🦙

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,23 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
+// console.log(employees);
+
 function isManager(id) {
-  // seu código aqui
+  return employees.some((emp) => emp.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId)) {
+    const managersEmp = employees.filter((emp) => emp.managers.includes(managerId));
+    return managersEmp.map((emp) => `${emp.firstName} ${emp.lastName}`);
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
+
+console.log(isManager('0e7b460e-acf4-4e17-bcb3-ee472265db83'));
+
+console.log(getRelatedEmployees('0e7b460e-acf4-4e17-bcb3-ee472265db83'));
 
 module.exports = { isManager, getRelatedEmployees };
