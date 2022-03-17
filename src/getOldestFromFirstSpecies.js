fb808fb53af7920f6971060923cbbb3ddf0a6bad
Airel <mv.ribeirosilva@gmail.com>
2022-03-16 22:22:03 -0300

    Requisito 9 🦝🐈‍🐈‍⬛

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
+const { species, employees } = data;
+
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const animalId = employees.find((emp) => emp.id === id).responsibleFor[0];
+  const animalList = species.find((anm) => anm.id === animalId).residents;
+  const animal = animalList.reduce((anm1, anm2) => (anm1.age > anm2.age ? anm1 : anm2));
+  return Object.values(animal);
 }
+
+console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 
 module.exports = getOldestFromFirstSpecies;
