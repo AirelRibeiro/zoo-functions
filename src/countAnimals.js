cdb6c590a8af200268a96024823d334606b75b68
Airel <mv.ribeirosilva@gmail.com>
2022-03-18 19:42:36 -0300

    Inclusao de comentarios

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,23 +1,23 @@
 const data = require('../data/zoo_data');
 
-const { species } = data;
+const { species } = data; // Traz o array species para ser usado nesse escopo
 
-function countAnimals(animal) {
-  if (!animal) {
-    const objeto = {};
-    species.forEach((spc) => {
-      objeto[spc.name] = spc.residents.length;
+function countAnimals(animal) { // Parâmetro é um objeto
+  if (!animal) { // Se o parametro 'animal' não existir
+    const objeto = {}; // Objeto vazio que será retornado
+    species.forEach(({ name, residents }) => { // forEach para percorrer o array species, usando o object destructuring para acessar as propriedades name e residents
+      objeto[name] = residents.length; // Cria uma chave com o name do animal e o número de residents
     });
-    return objeto;
+    return objeto; // Retorna o objeto após ser preenchido no forEach
   }
-  const anm = animal.specie;
-  const anms = species.find((spc) => spc.name === anm);
-  if (animal.sex) {
-    const anmSex = animal.sex;
-    const anmSexs = anms.residents.filter((res) => res.sex === anmSex);
-    return anmSexs.length;
+  const anm = animal.specie; // Armazena o valor contido na chave specie do objeto fornecido (que é o nome do animal ex.: 'lions')
+  const anms = species.find(({ name }) => name === anm); // Armazena como resultado o primeiro item cujo name corresponde ao anm
+  if (animal.sex) { // Condicional para verificar se o objeto contém a chave sex
+    const anmSex = animal.sex; // Armazena o valor contido na chave sex do objeto fornecido
+    const anmSexs = anms.residents.filter(({ sex }) => sex === anmSex); // Filtra e retorna um array com os animais residentes colocados em anms que possuem o sex pesquisado
+    return anmSexs.length; // Retorna o tamanho do array, ou seja a quantidade de animais correspondentes ao objeto pedido, com filtro sex
   }
-  return anms.residents.length;
+  return anms.residents.length; // Retorna o tamanho do array, ou seja a quantidade de animais correspondentes ao objeto pedido se não existir chave sex
 }
 
 console.log(countAnimals());
--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,48 +1,33 @@
 const data = require('../data/zoo_data');
 
-const { species } = data;
+const { species } = data; // Traz o array species para ser usado nesse escopo
 
-const retornoComLocalizacao = () => species.reduce((acc, { location, name }) => {
-  if (!acc[location]) {
-    acc[location] = [];
-    acc[location].push(name);
-  } else {
-    acc[location].push(name);
+const retornoComLocalizacao = () => species.reduce((acc, { location, name }) => { /* Function que usa reduce para gerar objeto caso não haja parâmetros ou includesnão exista; o segundo parâmetro usa object destructuring para recolher location e name do objetoatual */
+  if (!acc[location]) { // Verifica se a chave daquela location existe
+    acc[location] = []; // Cria a chave e atribui a ela um array vazio
+    acc[location].push(name); // Coloca dentro do array da location atual o name desse objeto
+  } else { // Caso a chave exista, njão entra no if e executa esse else
+    acc[location].push(name); // Coloca dentro do array da location atual o name desse objeto
   }
-  return acc;
+  return acc; // Retorna acc a cada iteração
 }, {});
 
-console.log(retornoComLocalizacao());
-
-const retornoComNome = (opc) => species.reduce((acc, { location, residents, name }) => {
-  let res = residents;
-  if (!acc[location]) {
-    acc[location] = [];
+const retornoComNome = (opc) => species.reduce((acc, { location, residents, name }) => { /* Function para gerar objeto a partir de qualquer especificação que seja passada como parâmetros em getAnimalMap, o segundo parâmetro usa object destructuring para recolher location, residents e name do objetoatual */
+  let res = residents; // let para armazenar o array de residents
+  if (!acc[location]) { // Se a chave com location do objeto não existir
+    acc[location] = []; // Cria a chave e atribui a ela um array vazio
   }
-  if (opc.sex) {
-    res = residents
-      .filter(({ sex }) => sex === opc.sex);
-  }
-  const res2 = res.map((anm) => anm.name);
-  if (opc.sorted) {
-    res2.sort();
-  }
-  acc[location].push({ [name]: res2 });
-  return acc;
+  if (opc.sex) res = residents.filter(({ sex }) => sex === opc.sex); // Se o objeto possuir a chave sex, atualiza a let res para ser um array com os animais apenas do sex fornecido
+  const resName = res.map((anm) => anm.name); // Coloca em uma uma constante o res passado pelo map, retornando um array com os 'names'
+  if (opc.sorted) resName.sort(); // Se o objeto possuir a chave sorted, ordena pela ordem alfabética, pelo padrão do método '.sort'
+  acc[location].push({ [name]: resName }); // Puxa para o array da chave location no objeto acc um objeto contendo name declarado na linha 15 e a const resName
 }, {});
-
-console.log(retornoComNome({ includeNames: true, sex: 'female' }));
 
 function getAnimalMap(options) {
-  if (!options || !options.includeNames) {
-    return retornoComLocalizacao();
+  if (!options || !options.includeNames) { // Se sem parâmetro ou sem includeNames
+    return retornoComLocalizacao(); // Executa retornoComLocalizacao
   }
-  return retornoComNome(options);
+  return retornoComNome(options); // Em qualquer outro caso, executa retornoComNome usando o que foi oferecido em options
 }
-
-console.log(getAnimalMap({ includeNames: true, sex: 'female', sorted: true }).NE);
-console.log(getAnimalMap({ includeNames: true, sex: 'female', sorted: true }).NW);
-console.log(getAnimalMap({ includeNames: true, sex: 'female', sorted: true }).SE);
-console.log(getAnimalMap({ includeNames: true, sex: 'female', sorted: true }).SW);
 
 module.exports = getAnimalMap;
